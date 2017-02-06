e89b46e191dd269f76485e84e19e758537431335
mxdi9i7 <peter.zheng88228@gmail.com>
2017-02-06 13:47:38 -0500

    post form successfully connected to mysql, working on file upload and permalink creation then store to mysql

--- routes/publish.js
+++ routes/publish.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 var knex = require('knex');
 
-
-
 const db = knex({
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
     user: 'root',
     database: 'test',
-  }
+  },
+  UseNullAsDefault: true
 })
 
 
@@ -25,6 +24,24 @@ router
           }
 				})
 		})
+  .post('/publish', function (req, res) {
+      const newEvent = {
+        title: req.body.eventName,
+        type: req.body.eventType,
+        date: '2017-01-01 19:00:00',
+        capacity: req.body.eventCapacity,
+        currentFill: 0,
+        address: req.body.eventAddress,
+        note: req.body.eventNote
+      }
+      db('events')
+        .insert(newEvent)
+        .then((ids) => {
+          newEvent.id = ids[0];
+          res.redirect('/event/'+newEvent.id);
+        })
+
+  });
 
 
 module.exports = router;
