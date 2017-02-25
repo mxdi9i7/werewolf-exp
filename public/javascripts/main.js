3c7dc1de7061a68e4788169f9cb288588dc1724d
mxdi9i7 <peter.zheng88228@gmail.com>
2017-02-25 00:02:53 -0500

    minor fix on publish page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('#A').text('狼人杀');
 	$('#B').text('三国杀');
 	$('#C').text('麻将');
+	$('#D').text('其他')
 	$('.carousel.carousel-slider').carousel({fullWidth: true});
 	$('.slider').slider();
 	$('select').material_select();
